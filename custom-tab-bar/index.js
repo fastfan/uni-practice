bf0639ac3c59cb1e84161b6206858df5f1f86ae3
weifan <1264682706@qq.com>
2025-05-15 08:44:49 +0800

    fix(*): :bug: *

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -6,13 +6,13 @@ Component({
 		selectedColor: "#FF7E7B",
 		backgroundColor: "#FFFFFF",
 		list: [{
-				pagePath: "/pages/index/index",
+				pagePath: "/pages/home/index/index",
 				text: "首页",
 				iconPath: "/static/images/tabbar/home.png",
 				selectedIconPath: "/static/images/tabbar/home_s.png"
 			},
 			{
-				pagePath: "/pages/charge/charge",
+				pagePath: "/pages/benefit/index/index",
 				text: "51充电",
 				iconPath: "/static/images/tabbar/mall.png",
 				selectedIconPath: "/static/images/tabbar/mall_s.png"
@@ -27,13 +27,13 @@ Component({
 			},
 			//#endif
 			{
-				pagePath: "/pages/mall/mall",
+				pagePath: "/pages/shop/index/index",
 				text: "商城",
 				iconPath: "/static/images/tabbar/mall.png",
 				selectedIconPath: "/static/images/tabbar/mall_s.png"
 			},
 			{
-				pagePath: "/pages/myzone/myzone",
+				pagePath: "/pages/mine/index/index",
 				text: "我的",
 				iconPath: "/static/images/tabbar/myzone.png",
 				selectedIconPath: "/static/images/tabbar/myzone_s.png"
@@ -51,10 +51,22 @@ Component({
 		switchTab(e) {
 			const data = e.currentTarget.dataset
 			const url = data.path
-			app.globalData.selected = data.index;
-			wx.switchTab({
-				url
-			})
+			const index = data.index
+			app.globalData.selected = index
+			// console.log(e)
+			if (index == 2) {
+				// 调起条码扫描
+				wx.scanCode({
+					success: function(res) {
+						console.log('条码类型：' + res.scanType)
+						console.log('条码内容：' + res.result)
+					}
+				})
+			} else {
+				wx.switchTab({
+					url
+				})
+			}
 		},
 	}
 })
